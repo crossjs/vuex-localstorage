10e4ce058c08bb139fe5b7d9d6fa3fe0ff203d9d
crossjs <liwenfu@crossjs.com>
2016-11-04 00:22:15 +0800

    build 0.3.0: add key to get/set

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 /**
  * createPersist
- * @param  {String} key             key
+ * @param  {String} namespace       namespace
  * @param  {Object} [initialState]  初始值/默认值
  * @param  {Object} [config]        自定义 provider/serialize/deserialize/expires
  * @return {Object}                 get/set 方法
  */
-export default function (key, initialState = {}, config) {
+export default function (namespace, initialState = {}, config) {
   const {
     provider,
     serialize,
@@ -18,24 +18,33 @@ export default function (key, initialState = {}, config) {
     expires: 0 // never expires
   }, config)
 
+  if (!namespace) {
+    namespace = rnd()
+  }
+
   return {
-    get () {
+    get (key = 'default') {
       let state
 
       try {
-        const { value, expires } = deserialize(provider.getItem(key))
+        const { value, expires } = deserialize(provider.getItem(`${namespace}-${key}`))
         if (expires === 0 || expires > Date.now()) {
+          // always a plain object
           state = deserialize(value)
         }
       } catch (e) {
         // console.log(e)
       }
 
-      return Object.assign({}, initialState, state)
+      return Object.assign({}, key === 'default' ? initialState : initialState[key], state)
     },
-    set (value) {
+    set (key, value) {
+      if (arguments.length === 1) {
+        value = key
+        key = 'default'
+      }
       try {
-        provider.setItem(key, serialize({
+        provider.setItem(`${namespace}-${key}`, serialize({
           value: serialize(value),
           expires: expires ? expires + Date.now() : expires
         }))
@@ -44,4 +53,8 @@ export default function (key, initialState = {}, config) {
       }
     }
   }
+}
+
+function rnd () {
+  return Date.now().toString(32) + Math.random().toString(32).slice(2)
 }
--- test/unit/specs/index.spec.js
+++ test/unit/specs/index.spec.js
@@ -1,36 +1,77 @@
 import createPersist from '../../../src'
 
+function rnd () {
+  return Date.now().toString(32) + Math.random().toString(32).slice(2)
+}
+
 describe('getPersist', () => {
   it('init', () => {
-    const persist = createPersist('KEY0')
+    const persist = createPersist(rnd())
     expect(persist.get()).to.eql({})
   })
 
   it('with initialState', () => {
-    const persist = createPersist('KEY1', { x: 1 })
+    const persist = createPersist(rnd(), { x: 1 })
     expect(persist.get()).to.eql({ x: 1 })
+  })
+
+  it('with empty namespace', () => {
+    const persist = createPersist(null, { x: 1 })
+    expect(persist.get()).to.eql({ x: 1 })
+  })
+
+  it('key should default to `default`', () => {
+    const persist = createPersist(null, { x: 1 })
+    expect(persist.get()).to.eql({ x: 1 })
+    expect(persist.get('default')).to.eql({ x: 1 })
   })
 })
 
 describe('setPersist', () => {
   it('with initialState', () => {
-    const persist = createPersist('KEY2', { z: 1 })
+    const persist = createPersist(rnd(), { z: 1 })
+    persist.set({ y: 1 })
+    expect(persist.get()).to.eql({ y: 1, z: 1 })
+  })
+
+  it('key should default to `default`', () => {
+    const persist = createPersist(rnd(), { z: 1 })
     persist.set({ y: 1 })
     expect(persist.get()).to.eql({ y: 1, z: 1 })
   })
 
   it('override initialState', () => {
-    const persist = createPersist('KEY3', { z: 1 })
+    const persist = createPersist(rnd(), { z: 1 })
     persist.set({ z: 0 })
     expect(persist.get()).to.eql({ z: 0 })
     persist.set({ z: null })
     expect(persist.get()).to.eql({ z: null })
   })
+
+  describe('with key', () => {
+    it('with initialState', () => {
+      const persist = createPersist(rnd(), { z: 1 })
+      persist.set('key1', { y: 1 })
+      expect(persist.get('key1')).to.eql({ y: 1 })
+      expect(persist.get()).to.eql({ z: 1 })
+    })
+
+    it('override initialState', () => {
+      const persist = createPersist(rnd(), { z: 1 })
+      expect(persist.get()).to.eql({ z: 1 })
+      persist.set('key2', { z: 0 })
+      expect(persist.get('key2')).to.eql({ z: 0 })
+      persist.set('key2', { z: null })
+      expect(persist.get('key2')).to.eql({ z: null })
+      persist.set('key3', { z: '' })
+      expect(persist.get('key3')).to.eql({ z: '' })
+    })
+  })
 })
 
 describe('customize', () => {
   it('with sessionStorage', () => {
-    const persist = createPersist('KEY4', { z: 1 }, {
+    const persist = createPersist(rnd(), { z: 1 }, {
       provider: sessionStorage
     })
     persist.set({ y: 1 })
@@ -39,7 +80,7 @@ describe('customize', () => {
 
   it('serialize/deserialize', () => {
     const value = {}
-    const persist = createPersist('KEY5', { z: 1 }, {
+    const persist = createPersist(rnd(), { z: 1 }, {
       provider: {
         getItem (key) {
           return value[key]
@@ -58,7 +99,7 @@ describe('customize', () => {
   })
 
   it('with expires', done => {
-    const persist = createPersist('KEY6', { z: 1 }, {
+    const persist = createPersist(rnd(), { z: 1 }, {
       // one second
       expires: 1000
     })
